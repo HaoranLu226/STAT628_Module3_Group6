dc8d57012ed65b98fd309ab6ec33d12081045f3b
Haoran Lu <hlu226@wisc.edu>
2020-12-02 17:24:11 -0600

    Re-organized files

--- code/wordcloud_tip.R
+++ code/wordcloud_tip.R
@@ -1,12 +1,14 @@
-# ------ Set-up
+# ------ Set-up ------
+
 rm(list=ls())
-#setwd("/Users/hrl/Desktop/628 M3/dataset")
+
+#setwd("../data")
 
 # install.packages("dplyr")
-# install.packages("tm")  # for text mining
-# install.packages("SnowballC") # for text stemming
-# install.packages("wordcloud") # word-cloud generator 
-# install.packages("RColorBrewer") # color palettes
+# install.packages("tm") 
+# install.packages("SnowballC")
+# install.packages("wordcloud")
+# install.packages("RColorBrewer") 
 
 library(tm)
 library(SnowballC)
@@ -14,83 +16,90 @@ library(wordcloud)
 library(RColorBrewer)
 library(dplyr)
 
-# ------
+# ------------ Cleaning data ------------
+
 tip <- read.csv("steakhouse_tip.csv")
 
-head(tip)
+#head(tip)
 text <- tip$text
-alltext <- paste(text,sep = "", collapse = " ")
+alltext <- paste(text,sep = "", collapse = " ") # combine all texts
 
-alltext <- gsub('[.]', '', alltext)
+alltext <- gsub('[.]', '', alltext) # remove marks
 alltext <- gsub('[,]', '', alltext)
 alltext <- gsub('[!]', '', alltext)
 alltext <- tolower(alltext)
-septext <- strsplit(alltext,split=' ')
 
-freq_0 <- table(septext)
-freq <- sort(freq_0, decreasing = TRUE)[1:100]
+septext <- strsplit(alltext,split=' ') # split words 
+
+# freq_0 <- table(septext)
+# freq <- sort(freq_0, decreasing = TRUE)[1:100]
+# head(septext)
 
 docs <- Corpus(VectorSource(septext))
-toSpace <- content_transformer(function (x , pattern) gsub(pattern, " ", x))
-docs <- tm_map(docs, toSpace, "/")
-docs <- tm_map(docs, toSpace, "@")
-docs <- tm_map(docs, toSpace, "\\|")
 
-docs <- tm_map(docs, content_transformer(tolower)) # Remove numbers
+docs <- tm_map(docs, removeWords, stopwords("english")) # remove stop words
 
-docs <- tm_map(docs, removeNumbers) # Remove english common stopwords
+docs <- tm_map(docs, removePunctuation) # remove punctuations
 
-docs <- tm_map(docs, removeWords, stopwords("english")) # Remove your own stop word
+docs <- tm_map(docs, stripWhitespace) # remove extra spaces
 
 
-docs <- tm_map(docs, removeWords, c("blabla1", "blabla2"))  # specify your stopwords as a character vector
+# ------------ Wordcloud ------------
 
-docs <- tm_map(docs, removePunctuation) # Remove punctuations
+set.seed(100)
 
-docs <- tm_map(docs, stripWhitespace) # Eliminate extra white spaces
+freqs <- sort(rowSums(as.matrix(TermDocumentMatrix(docs))),
+              decreasing=TRUE) 
+word_result <- data.frame(word = names(freqs),freq = freqs )
 
-
-dtm <- TermDocumentMatrix(docs)
-m <- as.matrix(dtm)
-v <- sort(rowSums(m),decreasing=TRUE)
-d <- data.frame(word = names(v),freq=v)
-head(d, 10)
-
-set.seed(1234)
-wordcloud(words = d$word, freq = d$freq, min.freq = 1,
+wordcloud(words = word_result$word, 
+          freq = word_result$freq, min.freq = 1,
           max.words=200, random.order=FALSE, rot.per=0.35, 
-          colors=brewer.pal(15, "Dark2"))
+          colors=brewer.pal(8, "Dark2"))
 
-# colors <- c('red','blue','green','maroon','brown','navy','purple')
-list_things <- list()
-list_other <- list()
+head(word_result)
+write.csv(word_result,"word_result.csv")
 
-list_food$word<-c("steak","bar","sushi","cheese",
-              "drinks","salad","meat","chicken","beer","wine","burger","hibachi","fresh",
-              "specials","crab","soup","fries","shrimp","bread","cocktails","rib","onion","lobster",
-              "sandwich","appetizer","fish","dessert","pork","lamb","tuna","salmon",
-              "chips","pizza","cinnamon","oysters","calamari","bisque","gluten","pepper","pie",
-              "italian","cheesecake","chocolate","mushrooms","nachos")
-list_food$freq <- (length(list_food$word):1)^1.5
+# ----------- Output -----------
+# Following groups are selected by our eyes from "word_result.csv"
+
+# === Food-realted ===
+
+list_food <- list()
+
+list_food$word<-c("steak","bar","sushi",
+              "drinks","salad","meat","chicken","beer","cheese",
+              "wine","burger","hibachi","fresh",
+              "specials","crab","soup","fries","bartender",
+              "shrimp","bread","cocktails","rib","onion","lobster",
+              "sandwich","appetizer","fish","dessert","pork",
+              "lamb","tuna","salmon","martini",
+              "chips","pizza","cinnamon","oysters","calamari",
+              "bisque","gluten","pepper","pie",
+              "italian","cheesecake","chocolate",
+              "mushrooms","nachos")
+list_food$freq <- length(list_food$word):1
 
 wordcloud(words = list_food$word, freq = list_food$freq, 
-          random.order=F,random.color=T, colors=brewer.pal(12, "Dark2"))
+          random.order=F,random.color=T, 
+          colors=brewer.pal(8, "Dark2"))
+
+# === Service-realted ===
+
+list_other <- list()
 
 list_other$word<-c("service","atmosphere",
-                  "lunch","dinner","night","wait","order","price","friendly","brunch",
-                  "reservation", "deliver","bartender","patio","music","breakfast","overpriced",
-                  "martini","clean","plates","wifi")
-list_other$freq <- (length(list_other$word):1)^1.2
+                  "lunch","dinner","night","wait","order",
+                  "price","friendly","brunch",
+                  "reservation", "deliver","patio",
+                  "music","breakfast","overpriced",
+                  "clean","plates","wifi")
+list_other$freq <- length(list_other$word):1
 
 wordcloud(words = list_other$word, freq = list_other$freq, 
-          random.order=F,random.color=T, colors=brewer.pal(12, "Set1"))
+          random.order=F,random.color=T, 
+          colors=brewer.pal(8, "Dark2"))
 
 
-# list_food$word<-c("steak","bar","sushi","cheese",
-#                   "drinks","salad","meat","chicken","beer","wine","burger","hibachi","fresh",
-#                   "specials","crab","soup","fries","shrimp","bread","cocktails","rib","onion","lobster",
-#                   "sandwich","appetizer","fish","dessert","pork","lamb","tuna","salmon",
-#                   "chips","pizza","cinnamon","oysters","calamari","bisque","gluten","pepper","pie",
-#                   "italian","cheesecake","chocolate","mushrooms","nachos")
-# list_food$freq <- (length(list_food$word):1) * 5 
+
 
